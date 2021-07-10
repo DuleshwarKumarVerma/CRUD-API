e89db7bee60c4501f5b8676c1beba3617a63f99b
Duleshwar <duleshwarv786@gmail.com>
2021-07-10 06:35:38 +0530

    final

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+// init the app
+const app = express()
+
+mongoose.connect('mongodb://localhost/naukri-khoje')
+mongoose.Promise = global.Promise
+
+
+app.use(bodyParser.json())
+
+app.use('/api', require('./routes/api'))
+
+app.use(function(err, req, res, next) {
+    res.status(422).send({ error: err.message })
+  })
+  
+
+app.listen(process.env.port || 4000, function() {
+  console.log('ready for accept request')
+})
